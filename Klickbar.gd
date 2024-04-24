fe77f4a24dee0dec4b401c3eab204a72a47672dd
maunzCache <kevin.kortum@gmx.de>
2024-04-24 20:04:09 +0000

    Neue Räume: Shelf room & Symbol room (#5)
    Fixt #3 und #4
    
    Reviewed-on: https://codeberg.org/maunzCache/ldjam-55/pulls/5
    Co-authored-by: maunzCache <kevin.kortum@gmx.de>
    Co-committed-by: maunzCache <kevin.kortum@gmx.de>

--- Klickbar.gd
+++ Klickbar.gd
@@ -2,5 +2,6 @@ extends Sprite2D
 
 func _input(event):
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
-		if get_rect().has_point(to_local(event.position)):
+		var local_event_position = to_local(event.position)
+		if get_rect().has_point(local_event_position) and is_pixel_opaque(local_event_position):
 			print("A click on the " + name)
