cc17fc0f8703c4c4734841c3adb81f0a9786274e
My name is Prince <Prince>
2024-04-24 22:32:43 +0200

    manueller merge

---
+++ Klickbar.gd
@@ -0,0 +1,7 @@
+extends Sprite2D
+
+func _input(event):
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		var local_event_position = to_local(event.position)
+		if get_rect().has_point(local_event_position) and is_pixel_opaque(local_event_position):
+			print("A click on the " + name)
